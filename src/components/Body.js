ef94e0dab6669c65d4fcfe721c36673ef21c620e
Rajeshwari Kondian <rajeshwarikondian@gmail.com>
2025-06-19 01:45:39 +0530

    useState hook used

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   // local State variable - super powerful variable
-  let [listOfRestaurant, setListOfRestaurant] = useState(resList)
+  let [listOfRestaurant, setListOfRestaurant] = useState([])
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+ const  fetchData = async() =>{
+  const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  );
 
+  const json = await data.json();
+
+      // View the structure in the console to locate restaurants
+  console.log(json);
+  setListOfRestaurant(json.cards[4].card.card.gridElemnts.infoWithStyle.restaurants)
+  
+ }
+  
  return (
     <div className="body">
       <div className="filter">
@@ -15,11 +29,10 @@ const Body = () => {
         onClick={()=>{
               //FILTER LOGIC HERE
             const filteredList = listOfRestaurant.filter(
-                (res) => res.info.avgRating > 4.3
+                (res) => res.info.avgRating > 4
               );
               setListOfRestaurant(filteredList)    
         }}
-      
         >Top rated restaurant </button>
       </div>
       <div className="res-container">
